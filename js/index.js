4d1eae033f4513f658568e46670eb6974981d244
Pavlo Dudar <dudar.pavloGmail.com>
2022-08-04 19:54:38 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -13,10 +13,9 @@
     let haveWinner = false;
     let arrayWin = [];
     let play = false;
-    let music = new Audio('../sounds/Music/MainTrack.mp3');
-    let resultFx = new Audio();
-    // let stepFx = new Audio('../sounds/FX/step.mp3');
-    let stepFx = new Audio();
+    let music = document.querySelector(".music");
+    let resultFx = document.querySelector(".resultFx");
+    let stepFx = document.querySelector(".stepFx");
     let winCombinations = [
         [1, 2, 3],
         [1, 4, 7],
@@ -35,8 +34,10 @@
     ];
     // Play button
     bt_play.addEventListener('click', function() {
-        stepFx.src = '../sounds/FX/step.mp3';
         stepFx.play();
+        resultFx.src = arraySoundsFx[0];
+        resultFx.volume = 0.0;
+        resultFx.play();
         if (!play) startMusic();
         play = true;
         const selectCharacter = Math.floor(Math.random() * 2);
@@ -75,10 +76,8 @@
     // User step
     function eventFunction(event) {
         const target = event.target;
-
         if (event.target.nodeName !== 'LI') return;
         if (!target.innerText) {
-            stepFx.src = '../sounds/FX/step.mp3';
             stepFx.play();
             maxSteps -= 1;
             target.innerText = user;
@@ -138,8 +137,8 @@
                             showModalWindow();
                             showResult();
                             resultColor();
-                            // resultFx.src = arraySoundsFx[0];
-                            resultFx.src = '../sounds/FX/win.mp3';
+                            resultFx.src = arraySoundsFx[0];
+                            resultFx.volume = 1;
                             resultFx.play();
                         }
                     }
@@ -148,8 +147,7 @@
                         if (countO >= 3) {
                             haveWinner = true;
                             userDate = [];
-                            // resultFx.src = arraySoundsFx[1];
-                            resultFx.src = '../sounds/FX/loose.mp3';
+                            resultFx.src = arraySoundsFx[1];
                             resultFx.play();
                             win = 'Computer Win!';
                             showModalWindow();
